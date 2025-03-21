d0881bc1f72e0a49981a310f4559d1fb3fc2976f
kinglove1990 <295856977@qq.com>
2025-03-21 13:20:12 +0800

    Update Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -26,23 +26,25 @@ fi
 if [ -d *"luci-theme-argon"* ]; then
 	cd ./luci-theme-argon/
 
-	sed -i '/font-weight:/ {/!important/! s/\(font-weight:\s*\)[^;]*;/\1normal;/}' $(find ./luci-theme-argon -type f -iname "*.css")
-	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/" ./luci-app-argon-config/root/etc/config/argon
-	
-        cd $PKG_PATH && echo "theme-argon has been fixed!"
+	sed -i "/font-weight:/ { /important/! { /\/\*/! s/:.*/: var(--font-weight);/ } }" $(find ./luci-theme-argon -type f -iname "*.css")
+	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
+
+	cd $PKG_PATH && echo "theme-argon has been fixed!"
 fi
 
 #修改qca-nss-drv启动顺序
 NSS_DRV="../feeds/nss_packages/qca-nss-drv/files/qca-nss-drv.init"
 if [ -f "$NSS_DRV" ]; then
- 	sed -i 's/START=.*/START=85/g' $NSS_DRV
+	sed -i 's/START=.*/START=85/g' $NSS_DRV
+
 	cd $PKG_PATH && echo "qca-nss-drv has been fixed!"
 fi
 
 #修改qca-nss-pbuf启动顺序
 NSS_PBUF="./kernel/mac80211/files/qca-nss-pbuf.init"
 if [ -f "$NSS_PBUF" ]; then
- 	sed -i 's/START=.*/START=86/g' $NSS_PBUF
+	sed -i 's/START=.*/START=86/g' $NSS_PBUF
+
 	cd $PKG_PATH && echo "qca-nss-pbuf has been fixed!"
 fi
 
@@ -77,5 +79,6 @@ fi
 CM_FILE=$(find ../feeds/packages/ -maxdepth 3 -type f -wholename "*/coremark/Makefile")
 if [ -f "$CM_FILE" ]; then
 	sed -i 's/mkdir/mkdir -p/g' $CM_FILE
+
 	cd $PKG_PATH && echo "coremark has been fixed!"
 fi
